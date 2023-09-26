e92f8ac279a196eefcd113b6bb970f7b4af14274
lisette900809 <lgonzalez@zertifika.com>
2023-09-26 00:44:45 -0400

    display extracted data from image

--- pages/upload-image.vue
+++ pages/upload-image.vue
@@ -4,7 +4,8 @@ const isUpload = ref(false);
 const isLoading = ref(false);
 const imgFile = ref(null);
 const imgPreview = ref(null);
-const text = ref(null);
+const extractedData = ref(null);
+const responseSuccess = ref(false);
 const activeTab = ref("device");
 const errorAlert = ref(false);
 const errorMessage = ref(null);
@@ -15,6 +16,7 @@ const allowedTypes = ref([
   "image/heic",
   "image/tiff",
 ]);
+const invoiceData = ref(null);
 
 function onChange(e) {
   imgFile.value = e.files[0];
@@ -46,9 +48,37 @@ async function uploadImage() {
   });
 
   if (data.value.api_request.status_code == 201) {
-    text.value = data.value.document.inference.prediction;
+    extractedData.value = data.value.document.inference.prediction;
+    const invoiceUpdatedKeys = {
+      customer_address: "Customer address",
+      customer_company_registrations: "Customer registration",
+      customer_name: "Customer",
+      date: "Date",
+      due_date: "Due date",
+      document_type: "Tipo de documento",
+      locale: "Language",
+      supplier_name: "Supplier",
+      supplier_address: "Supplier address",
+      supplier_company_registrations: "Supplier registration",
+      invoice_number: "Invoice number",
+      total_amount: "Total amount",
+      total_net: "Total net",
+    };
+
+    invoiceData.value = renameKeys(invoiceUpdatedKeys, extractedData.value);
     isLoading.value = false;
+    responseSuccess.value = true;
   }
+}
+
+function renameKeys(keysMap, obj) {
+  return Object.keys(obj).reduce(
+    (acc, key) => ({
+      ...acc,
+      ...{ [keysMap[key] || key]: obj[key] },
+    }),
+    {}
+  );
 }
 
 function cancelUpload() {
@@ -69,12 +99,12 @@ function changeTab(tabName) {
 
 <template>
   <section>
-    <div class="container">
-      <div class="row my-5 justify-content-center">
-        <div class="col-md-6">
-          <AppAlert v-if="errorAlert" :errorMessage="errorMessage" />
-          <div class="card upload-card mt-4 p-4">
-            <div class="row mt-2 mb-5 text-center" v-if="!isUpload">
+    <div class="container mt-5">
+      <AppAlert v-if="errorAlert" :errorMessage="errorMessage" />
+      <div class="card upload-card p-4">
+        <div class="row justify-content-center">
+          <div class="col-md-6" v-if="!isUpload">
+            <div class="row mt-4 mb-5 text-center">
               <h5 class="mb-4">Select document type you want process</h5>
               <div class="col">
                 <div class="card doc-type-card type-active">
@@ -93,53 +123,15 @@ function changeTab(tabName) {
               </div>
             </div>
             <ul class="nav nav-tabs justify-content-end">
-              <li
-                class="nav-item cs-pointer"
-                v-if="!isUpload"
-                @click="changeTab('device')"
-              >
+              <li class="nav-item cs-pointer" @click="changeTab('device')">
                 <a class="nav-link" :class="{ active: activeTab == 'device' }"
                   >Upload</a
                 >
               </li>
-              <li
-                class="nav-item cs-pointer"
-                v-if="!isUpload"
-                @click="changeTab('link')"
-              >
+              <li class="nav-item cs-pointer" @click="changeTab('link')">
                 <a class="nav-link" :class="{ active: activeTab == 'link' }"
                   >From URL</a
                 >
-              </li>
-              <li
-                class="nav-item cs-pointer"
-                v-if="isUpload"
-                @click="uploadImage"
-              >
-                <div class="nav-link">
-                  <i class="bi-upload pe-1"></i> Upload
-                  <span
-                    class="spinner-border text-primary spinner-border-sm ms-1"
-                    role="status"
-                    aria-hidden="true"
-                    v-if="isLoading"
-                  ></span>
-                </div>
-              </li>
-              <li
-                class="nav-item cs-pointer"
-                v-if="isUpload"
-                @click="cancelUpload"
-              >
-                <div class="nav-link">
-                  <i class="bi-x-circle pe-1 text-danger"></i> Cancelar
-                  <span
-                    class="spinner-border text-primary spinner-border-sm ms-1"
-                    role="status"
-                    aria-hidden="true"
-                    v-if="isLoading"
-                  ></span>
-                </div>
               </li>
             </ul>
             <div class="card-body">
@@ -181,14 +173,84 @@ function changeTab(tabName) {
                   </div>
                 </div>
               </div>
-              <UploadByLink
-                @typedUrl="getUrl"
-                v-if="activeTab == 'link' && !isUpload"
-              />
             </div>
-            <div v-if="isUpload">
-              <img :src="imgPreview" class="img-fluid px-5 py-3" />
+          </div>
+          <div class="col-md-5" v-if="isUpload">
+            <UploadByLink
+              @typedUrl="getUrl"
+              v-if="activeTab == 'link' && !isUpload"
+            />
+            <ul
+              class="nav nav-tabs justify-content-end"
+              v-if="!responseSuccess"
+            >
+              <li class="nav-item cs-pointer" @click="uploadImage">
+                <div class="nav-link">
+                  <i class="bi-upload pe-1"></i> Upload
+                  <span
+                    class="spinner-border text-primary spinner-border-sm ms-1"
+                    role="status"
+                    aria-hidden="true"
+                    v-if="isLoading"
+                  ></span>
+                </div>
+              </li>
+              <li class="nav-item cs-pointer" @click="cancelUpload">
+                <div class="nav-link">
+                  <i class="bi-x-circle pe-1 text-danger"></i> Cancel
+                </div>
+              </li>
+            </ul>
+            <div>
+              <img :src="imgPreview" class="img-fluid py-3" />
             </div>
+          </div>
+          <div class="col-md-3 my-4 extracted-data" v-if="responseSuccess">
+            <ul class="list-group rounded-0">
+              <div v-for="(text, key, index) in invoiceData" :key="index">
+                <div v-if="text.value != null">
+                  <div class="fw-bold key-name">{{ key }}</div>
+                  <li class="list-group-item mb-3">
+                    <div>{{ text.value }}</div>
+                  </li>
+                </div>
+              </div>
+            </ul>
+          </div>
+          <div class="col-md-4 mt-4 extracted-data" v-if="responseSuccess">
+            <ul class="list-group rounded-0">
+              <div class="fw-bold key-name">Taxes</div>
+              <li
+                class="list-group-item"
+                v-for="(tax, index) in invoiceData.taxes"
+                :key="index"
+              >
+                <div>{{ tax.rate }}% - {{ tax.value }}</div>
+              </li>
+            </ul>
+            <ul class="list-group rounded-0">
+              <div class="fw-bold key-name">PO #</div>
+              <li
+                class="list-group-item"
+                v-for="(reference, index) in invoiceData.reference_numbers"
+                :key="index"
+              >
+                <div>{{ reference.value }}</div>
+              </li>
+            </ul>
+            <ul class="list-group rounded-0">
+              <div class="fw-bold key-name">Line Items</div>
+              <li
+                class="list-group-item mb-3"
+                v-for="(item, index) in invoiceData.line_items"
+                :key="index"
+              >
+                <div>
+                  {{ item.quantity || 1 }}- {{ item.description }} x
+                  {{ item.unit_price }}.00 - {{ item.total_amount }}.00
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
