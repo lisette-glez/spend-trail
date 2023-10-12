c66bdf0977549c44323a89692c4507547c04a8c5
lisette900809 <lgonzalez@zertifika.com>
2023-10-12 00:31:18 -0400

    add project layout

---
+++ components/TheFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="fixed-bottom bg-white">
+    <div class="row my-2 d-flex justify-content-between">
+      <div class="col text-center pt-2">
+        <span>Copyright &copy; Image2Text {{ new Date().getFullYear() }}</span>
+      </div>
+      <div class="col-md-4 text-center">
+        <ul class="nav">
+          <li class="nav-item">
+            <NuxtLink
+              to="https://github.com/lisette900809/image2text"
+              target="_blank"
+              class="nav-link"
+              ><i class="bi bi-github h4"></i
+            ></NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink
+              to="https://www.linkedin.com/in/lisette-gonzalez/"
+              target="_blank"
+              class="nav-link"
+              ><i class="bi bi-linkedin h4"></i
+            ></NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink to="https://lisette.dev" target="_blank" class="nav-link"
+              ><i class="bi bi-link-45deg h4"></i
+            ></NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-expand bg-white py-3 shadow-sm">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item cs-pointer me-3">
+        <NuxtLink to="/" class="nav-link"
+          ><span class="text-primary">About</span></NuxtLink
+        >
+      </li>
+      <div class="topbar-divider d-none d-sm-block"></div>
+      <li class="nav-item cs-pointer me-3">
+        <NuxtLink to="/upload-image" class="nav-link"
+          ><span class="text-primary">Live demo</span></NuxtLink
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
